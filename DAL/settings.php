b39456b29284b4cbcb86ed899ef9d1dece28b61f
Werdlem <smrobins@virginmedia.com>
2018-03-01 07:48:39 +0000

    v18.04

--- DAL/settings.php
+++ DAL/settings.php
@@ -1,7 +1,7 @@
 <?php
  
-//define('DB_HOST', 'localhost');define('DB_NAME', 'tooling'); define('DB_USER', 'root');define('DB_PASSWORD', 'root');
-define('DB_HOST', 'localhost');define('DB_NAME', 'tooling'); define('DB_USER', 'root');define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');define('DB_NAME', 'tooling'); define('DB_USER', 'root');define('DB_PASSWORD', 'root');
+//define('DB_HOST', 'localhost');define('DB_NAME', 'tooling'); define('DB_USER', 'root');define('DB_PASSWORD', '');
 //define('DB_HOST', 'sqlserver2'); define('DB_NAME', 'ppkstock'); define('DB_USER', 'ppkstock'); define('DB_PASSWORD', 'ppkstock');
 
 define('SMTP_HOST', 'mail');

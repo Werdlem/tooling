24b85e8eb3ba8cdfc2a736bb63b11e984427c715
Werdlem <smrobins@virginmedia.com>
2018-08-23 19:17:56 +0100

    Update settings.php

--- DAL/settings.php
+++ DAL/settings.php
@@ -2,9 +2,9 @@
  
 //define('DB_HOST', 'localhost');define('DB_NAME', 'tooling'); define('DB_USER', 'root');define('DB_PASSWORD', 'root'); //work sql connecton
 
-define('DB_HOST', 'localhost');define('DB_NAME', 'tooling'); define('DB_USER', 'root');define('DB_PASSWORD', '');// home sql connection
+//define('DB_HOST', 'localhost');define('DB_NAME', 'tooling'); define('DB_USER', 'root');define('DB_PASSWORD', '');// home sql connection
 
-//define('DB_HOST', 'sqlserver2'); define('DB_NAME', 'ppkstock'); define('DB_USER', 'ppkstock'); define('DB_PASSWORD', 'ppkstock'); //postpack.web sql connection
+define('DB_HOST', 'sqlserver2'); define('DB_NAME', 'ppkstock'); define('DB_USER', 'ppkstock'); define('DB_PASSWORD', 'ppkstock'); //postpack.web sql connection
 
 define('SMTP_HOST', 'mail');
 define('SMTP_PORT', 25);

d33e8de855e8dc98476272b1b8edb22bef172994
Werdlem <smrobins@virginmedia.com>
2019-01-08 16:31:39 +0000

    v18.32a

--- templates/searchCustomer.php
+++ templates/searchCustomer.php
@@ -5,14 +5,14 @@
 	
 
 <h1>Search Customers</h1>
-<p><input type="" ng-model=""> <button type="button" class="btn btn-info btn-sml" ng-click="searchCustomer()">Search</button></p>
+<p><input type="" ng-model="search"> <button type="button" class="btn btn-info btn-sml" ng-click="searchCustomer()">Search</button></p>
 	<br/>
 <span ng-model="search" ng-init="search='a'" ng-click="searchCustomer()">A</span>
 <span ng-model="search" ng-init="search='p'" ng-click="searchCustomer()">P</span>
 BCDEFGHIJKLMNOPQRSTUVWQYZ
 
-<span ng-repeat="y in c.allCustomers">
-	{{y.customer(0)}}</span>
+<span ng-repeat="">
+	</span>
 	<table class="table table-sm">
 		
 		<thead>
@@ -20,7 +20,7 @@ BCDEFGHIJKLMNOPQRSTUVWQYZ
 			<th>Business</th>
 			<th>Contact No</th>
 		</thead>
-		<tr ng-repeat="x in c.customers">
+		<tr ng-repeat="">
 			<td> <a href="/customers?customer={{x.customer}}&id={{x.id}}">{{x.customer}}</a></td>
 			<td>{{x.business}}</td>
 			<td>{{x.contact_no}}</td>

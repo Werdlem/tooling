3796149db115ae7c9aa9ddecf5536ccbeba75362
Werdlem <smrobins@virginmedia.com>
2019-04-08 11:20:21 +0100

    v19.01

--- restricted/myApp.js
+++ restricted/myApp.js
@@ -362,7 +362,7 @@ myApp.controller('customer', function($scope,$http,$location){
         salesId:$scope.newQuote.details.sales_man.salesId,
         salesInitials:$scope.newQuote.details.sales_man.initials}
       }).then((response)=>{
-        //window.location.replace("/customerQuote")
+        window.location.replace("/customerQuote")
       });
     }
     $http({

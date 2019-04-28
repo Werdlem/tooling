6cee15e3357b07d3492f83b7d2c2ade243996b95
Werdlem <smrobins@virginmedia.com>
2019-04-28 21:50:43 +0100

    Update getToolsById.json.php

--- jsonData/getToolsById.json.php
+++ jsonData/getToolsById.json.php
@@ -1,11 +1,8 @@
 <?php 
 
 require_once ('../DAL/DBConn.php');
-$data = json_decode(file_get_contents("php://input"));
-$sales = $data->sales;
-$status = $data->status;
-$dateFrom = $data->dateFrom;
-$dateTo = $data->dateTo;
+$id = json_decode(file_get_contents("php://input"));
+
 $dal = new tooling();
 $fetch = $dal->getToolById($id);
 print (json_encode($fetch));

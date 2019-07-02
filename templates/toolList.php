ac387b08784913bc71030f8c4046473fff6e190c
Werdlem <smrobins@virginmedia.com>
2019-07-02 10:44:49 +0100

    v19.27

--- templates/toolList.php
+++ templates/toolList.php
@@ -1,8 +1,10 @@
 <div ng-controller="toolList as tl">
 
 	<div id="filter" style="border: 1px solid #777; width: auto; text-align: center; float: left; padding: 5px">
-        <label>Filter Tool<br/>
-       <input ng-model="search.tool_ref" ng-model="search.tool_alias" style="width: 10em" autofocus="autofocus"></label>
+        <label style="position: left;">Filter Ref: 
+       <input ng-model="search.tool_ref" style="width: 10em" autofocus="autofocus"></label>
+   		<label style="text-align:left;">Filter All: <input ng-model="find" style="width: 10em; float: right;" ></label>
+       
     </div> 
 
 <table class="table">
@@ -20,7 +22,7 @@
 		<th>KTOK Width</th>
 		<th>KTOK Length</th>
 </thead>
-<tr ng-repeat="x in tl.getToolsList | filter:search">
+<tr ng-repeat="x in tl.getToolsList | filter:search |filter:find">
 	<td><a href="/toolQuote?id={{x.id}}">{{x.tool_ref}}</a></td>
 	<td>{{x.tool_alias}}</td>
 	<td>{{x.config}} UP</td>

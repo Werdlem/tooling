22ac2e3d9f8d9fc11fc393becea4882de89a4f63
Werdlem <smrobins@virginmedia.com>
2020-04-20 12:16:21 +0100

    ncr
    added page refresh button to ncr

--- templates/ncr.php
+++ templates/ncr.php
@@ -28,5 +28,6 @@
 	<textarea ng-model="description" ng-change="updateLine(reason,description, x)" placeholder="Please give short description of non-conformance" ></textarea></td></div>   		
 	</tr>
 	</table>
-	
+	<input type="button" name="" onClick="window.location.reload()" value="Save Quote">
+	</script>
 	</div>

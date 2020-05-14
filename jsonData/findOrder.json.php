780acf442ccf9757332289a5c7456bef7b2687d9
Werdlem <smrobins@virginmedia.com>
2020-05-14 13:37:13 +0100

    ncr fix

--- jsonData/findOrder.json.php
+++ jsonData/findOrder.json.php
@@ -3,8 +3,8 @@
 require_once ('../DAL/DBConn2.php');
 $dal = new tartarus();
 $data = json_decode(file_get_contents("php://input"));
-//$order = $data->order;
-$order = 'p237131';
+$order = $data->order;
+//$order = 'p237131';
 $fetch = $dal->findOrder($order);
 
-print_r($fetch);
+echo json_encode($fetch);
